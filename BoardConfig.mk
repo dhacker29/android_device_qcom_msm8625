093c5b1d7e3b96b89e0daefe50a6a3241841f65c
dhacker29 <dhackerdvm@gmail.com>
2014-03-30 17:05:09 -0400

    Add recovery configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,3 +11,14 @@ include device/qcom/msm7627a/BoardConfig.mk
 KERNEL_DEFCONFIG := cyanogenmod_normandy_defconfig
 BOARD_NO_SPEAKER :=
 TARGET_BOOTIMG_SIGNED :=
+
+# recovery
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun%d/file
+TARGET_RECOVERY_INITRC := device/qcom/msm8625/recovery.rc
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+DEVICE_RESOLUTION := 480x800
+TW_FLASH_FROM_STORAGE := true
+TW_INTERNAL_STORAGE_PATH := "/sdcard1"
+TW_EXTERNAL_STORAGE_PATH := "/sdcard"
+TW_DEFAULT_EXTERNAL_STORAGE := true
