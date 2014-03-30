dc774c65b77354ef37455336261fc674999771de
dhacker29 <dhackerdvm@gmail.com>
2014-03-30 16:03:50 -0400

    Update fstab for nokia partitions

--- msm8625.mk
+++ msm8625.mk
@@ -2,3 +2,8 @@ include device/qcom/msm7627a/msm7627a.mk
 
 PRODUCT_NAME := msm8625
 PRODUCT_DEVICE := msm8625
+
+PRODUCT_COPY_FILES += \
+    device/qcom/msm8625/vold.fstab:/system/etc/vold.fstab
+
+$(call inherit-product-if-exists, vendor/nokia/normandy/normandy-vendor.mk)
