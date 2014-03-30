c6cf577fe6e735aa0ceb656636053000bdb6b20e
dhacker29 <dhackerdvm@gmail.com>
2014-03-30 16:51:08 -0400

    Define defconfig unset some vars

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,3 +7,7 @@
 include device/qcom/msm7627a/BoardConfig.mk
 
 -include vendor/qcom/proprietary/common/msm8625/BoardConfigVendor.mk
+
+KERNEL_DEFCONFIG := cyanogenmod_normandy_defconfig
+BOARD_NO_SPEAKER :=
+TARGET_BOOTIMG_SIGNED :=
