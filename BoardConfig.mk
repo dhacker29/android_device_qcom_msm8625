9515ce37fa544dbdc09aeb7a5cd22e9d4d543c67
dhacker29 <dhackerdvm@gmail.com>
2014-04-03 04:56:55 -0400

    updates

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,11 +12,15 @@ KERNEL_DEFCONFIG := cyanogenmod_normandy_defconfig
 BOARD_NO_SPEAKER :=
 TARGET_BOOTIMG_SIGNED :=
 
-# recovery
+# Recovery
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun%d/file
-TARGET_RECOVERY_INITRC := device/qcom/msm8625/recovery.rc
 BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_CUSTOM_GRAPHICS := ../../../device/nokia/normandy/recovery/graphics.c
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_BOARD_CUSTOM_GRAPHICS := ../../../device/nokia/normandy/recovery/tw_graphics.c
+TW_TARGET_USES_QCOM_BSP := true
+TARGET_RECOVERY_FSTAB := device/nokia/normandy/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_INITRC := device/nokia/normandy/recovery/recovery.rc
 DEVICE_RESOLUTION := 480x800
 TW_FLASH_FROM_STORAGE := true
 TW_INTERNAL_STORAGE_PATH := "/sdcard1"
--- msm8625.mk
+++ msm8625.mk
@@ -4,6 +4,7 @@ PRODUCT_NAME := msm8625
 PRODUCT_DEVICE := msm8625
 
 PRODUCT_COPY_FILES += \
-    device/qcom/msm8625/vold.fstab:/system/etc/vold.fstab
+    device/qcom/msm8625/vold.fstab:/system/etc/vold.fstab \
+    device/qcom/msm8625/recovery_nand.fstab:/system/etc/recovery_nand.fstab
 
 $(call inherit-product-if-exists, vendor/nokia/normandy/normandy-vendor.mk)
