8168d8401a9473905534991573ca005df5ff0a15
dhacker29 <dhackerdvm@gmail.com>
2014-04-04 02:47:10 -0400

    Fixes for sdcards

--- msm8625.mk
+++ msm8625.mk
@@ -1,10 +1,14 @@
 include device/qcom/msm7627a/msm7627a.mk
 
+DEVICE_PACKAGE_OVERLAYS += device/qcom/msm8625/overlay
+
 PRODUCT_NAME := msm8625
 PRODUCT_DEVICE := msm8625
 
 PRODUCT_COPY_FILES += \
-    device/qcom/msm8625/vold.fstab:/system/etc/vold.fstab \
-    device/qcom/msm8625/recovery_nand.fstab:/system/etc/recovery_nand.fstab
+    device/qcom/msm8625/init.qcom.rc:/root/init.qcom.rc \
+    device/qcom/msm8625/recovery_nand.fstab:/system/etc/recovery_nand.fstab \
+    device/qcom/msm8625/ueventd.qcom.rc:/root/ueventd.qcom.rc \
+    device/qcom/msm8625/vold.fstab:/system/etc/vold.fstab
 
 $(call inherit-product-if-exists, vendor/nokia/normandy/normandy-vendor.mk)
