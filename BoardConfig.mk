c125de098f99575609bba4cb49d5a33353085c8d
dhacker29 <dhackerdvm@gmail.com>
2014-04-07 05:32:24 -0400

    More fixes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,7 @@ include device/qcom/msm7627a/BoardConfig.mk
 -include vendor/qcom/proprietary/common/msm8625/BoardConfigVendor.mk
 
 KERNEL_DEFCONFIG := cyanogenmod_normandy_defconfig
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.emmc=true loglevel=1 vmalloc=200M
 BOARD_NO_SPEAKER :=
 TARGET_BOOTIMG_SIGNED :=
 TARGET_NO_BOOTLOADER := true
