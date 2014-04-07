0b64d284550781564532ad89baf9cefae1ca6ac3
dhacker29 <dhackerdvm@gmail.com>
2014-04-07 06:36:38 -0400

    Fix header copy

--- Android.mk
+++ Android.mk
@@ -1,4 +1,10 @@
-#
-# This empty Android.mk file exists to prevent the build system from
-# automatically including any other Android.mk files under this directory.
-#
+ifeq ($(TARGET_DEVICE),msm8625)
+
+include $(CLEAR_VARS)
+
+LOCAL_COPY_HEADERS_TO         := $(TARGET_OUT_HEADERS)/loc_api/rpcgen/inc
+LOCAL_COPY_HEADERS            := ../../../device/qcom/msm8625/commondefs_rpcgen_rpc.h
+#Copy the headers for gps
+include $(BUILD_COPY_HEADERS)
+
+endif
--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -31,10 +31,3 @@ $(LOCAL_BUILT_MODULE):
 	$(hide) rm -rf $(SYMLINK)
 	$(hide) ln -sf $(TARGET) $(SYMLINK)
 	$(hide) touch $@
-
-include $(CLEAR_VARS)
-
-LOCAL_COPY_HEADERS_TO         := $(TARGET_OUT_HEADERS)/loc_api/rpcgen/inc
-LOCAL_COPY_HEADERS            := ../../../device/qcom/msm8625/commondefs_rpcgen_rpc.h
-#Copy the headers for gps
-include $(BUILD_COPY_HEADERS)
